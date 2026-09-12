58e250cba8557debce011f6b2264e43bdc62d4b6
Ishwar Upadhye <upadhyeishwar@gmail.com>
2026-09-12 19:52:13 +0530

    Add multiple video entries to videos.js
    Added multiple video entries with titles, links, and thumbnails.

--- videos.js
+++ videos.js
@@ -15,6 +15,37 @@ const VIDEOS = [
     link: "https://drive.google.com/file/d/1HP__avu1krPifBi5NXWuN0A-D3Wn-oom/view?usp=drive_link",
     thumbnail: "https://image.tmdb.org/t/p/original/p4AqYjv70O1KjKA47x1Tk5i4djL.jpg"
   },
+const VIDEOS = [
+  {
+    title: "The Shawshank Redemption",
+    link: "https://drive.google.com/file/d/1-hHQWQ6zPfnAESIToJAVgyYhLKEJh6kj/view?usp=drive_link",
+    thumbnail: "https://image.tmdb.org/t/p/w600_and_h900_face/9cqNxx0GxF0bflZmeSMuL5tnGzr.jpg"
+  },
+const VIDEOS = [
+  {
+    title: "Se7ven",
+    link: "https://drive.google.com/file/d/1DLVdxDE-DwnntovSqzf0F9_mzoMaGu7_/view?usp=drive_link",
+    thumbnail: "https://image.tmdb.org/t/p/w600_and_h900_face/191nKfP0ehp3uIvWqgPbFmI4lv9.jpg"
+  },
+const VIDEOS = [
+  {
+    title: "Fight Club",
+    link: "https://drive.google.com/file/d/16UFOxYfwIMa99ZxAnG0Cc8E0rMbULh2f/view?usp=drive_link",
+    thumbnail: "https://image.tmdb.org/t/p/w600_and_h900_face/jSziioSwPVrOy9Yow3XhWIBDjq1.jpg"
+  },
+const VIDEOS = [
+  {
+    title: "Life of Pie",
+    link: "https://drive.google.com/file/d/1-6erWd-jWPaOMgl3Co9M7tCX0id26lJO/view?usp=drive_link",
+    thumbnail: "https://image.tmdb.org/t/p/w600_and_h900_face/iLgRu4hhSr6V1uManX6ukDriiSc.jpg"
+  },
+const VIDEOS = [
+  {
+    title: "The Dark Knight",
+    link: "https://drive.google.com/file/d/1EwnPUCJwqfKMnrd9nsW8njlIdPpgNjbE/view?usp=drive_link",
+    thumbnail: "https://image.tmdb.org/t/p/w600_and_h900_face/qJ2tW6WMUDux911r6m7haRef0WH.jpg"
+  },
+
 
   // Copy the block above and paste it below to add more, e.g.:
   // {
